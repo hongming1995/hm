8c78e226702c99e5353692c9749b0a25a0942679
hongming <1570140496@qq.com>
2021-08-23 09:11:16 +0800

    ThreadPoolExecutor线程池使用

--- src/main/java/com/example/designMode/MyThreadPoolExecutor/MyThreadPool.java
+++ src/main/java/com/example/designMode/MyThreadPoolExecutor/MyThreadPool.java
@@ -8,6 +8,14 @@ public class MyThreadPool {
 
     public static Map<String, ThreadPoolExecutor> threadMap = new HashMap<>();
 
+    /**
+     * 创建线程池
+     * 队列的实现类：
+     * 1.ArrayBlockingQueue：有序的队列，且有界，存在缓冲队列
+     * 2.LinkedBlockingQueue：有序的队列，且无界(其实为int最大值)，存在缓冲队列
+     * 3.SynchronousQueue：无界，且不存在缓冲的队列
+     * @return
+     */
     public static ThreadPoolExecutor getPool() {
         return new ThreadPoolExecutor(ThreadBody.corePoolSize,
                 ThreadBody.maximumPoolSize,
