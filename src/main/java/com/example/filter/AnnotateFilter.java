26ed4656d2f14c0ec75b81337c6c697c3ff665a7
hongming <1570140496@qq.com>
2023-09-23 19:49:16 +0800

    Filter过滤器

---
+++ src/main/java/com/example/filter/AnnotateFilter.java
@@ -0,0 +1,12 @@
+package com.example.filter;
+
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * @Author: hongming
+ * @Date: 2023/09/23/19:41
+ * @Description:
+ */
+/*@WebFilter(filterName = "annotateFilter", urlPatterns = "/*")*/
+public class AnnotateFilter {
+}
---
+++ src/main/java/com/example/filter/MyFilter.java
@@ -0,0 +1,26 @@
+package com.example.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @Author: hongming
+ * @Date: 2023/09/23/19:19
+ * @Description:
+ */
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
