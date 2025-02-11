198d90437c33e88e9df2df672e7ce003eb0098e6
hongming <1570140496@qq.com>
2025-02-11 21:45:25 +0800

    java基础知识测试

---
+++ src/main/java/com/example/javabase/MyCollectionOnlyRead.java
@@ -0,0 +1,19 @@
+package com.example.javabase;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @Author: hongming
+ * @Date: 2025/02/10/22:16
+ * @Description:
+ */
+public class MyCollectionOnlyRead {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        Collection<String> modifyList = Collections.unmodifiableCollection(list);
+        modifyList.add("b");
+    }
+}
---
+++ src/main/java/com/example/javabase/MyIterator.java
@@ -0,0 +1,30 @@
+package com.example.javabase;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * @Author: hongming
+ * @Date: 2025/02/10/22:04
+ * @Description:
+ */
+public class MyIterator {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String next = iterator.next();
+            if (StringUtils.equals("d", next)) {
+                iterator.remove();
+                continue;
+            }
+            System.out.println(next);
+        }
+    }
+}
